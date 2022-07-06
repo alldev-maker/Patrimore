34e24c632261a697f3de5030c0258f808738636a
Gabriel7096 <gabriel.arias.sh@gmail.com>
2022-07-06 14:32:09 -0500

    feat/change scroll bar style

--- gatsby-node.js
+++ gatsby-node.js
@@ -26,10 +26,10 @@ exports.createPages = async ({ graphql, actions }) => {
 }
 
 exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
-  if (stage === 'build-javascript') {
+  if (stage === "build-javascript") {
     const config = getConfig()
     const miniCssExtractPlugin = config.plugins.find(
-      plugin => plugin.constructor.name === 'MiniCssExtractPlugin'
+      plugin => plugin.constructor.name === "MiniCssExtractPlugin"
     )
     if (miniCssExtractPlugin) {
       miniCssExtractPlugin.options.ignoreOrder = true
